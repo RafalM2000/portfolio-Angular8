c58f9be91608a788c1daa24b6cd5eb9e8d814d7a
Rafal <rmf@europe.com>
2019-09-25 21:58:36 +0200

    ngClass direcive

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -11,12 +11,13 @@ usersList = ['Mike', 'Jon', 'Sara', 'Eva'];
 isVisible = true;
 switchExpression = 'No2';
 colorConverter = false;
+currencies =  ['PLN', 'GBP', 'EUR', 'USD'];
+currencyItem = 0;
 
   constructor() { }
 
   ngOnInit() {
   }
-
   onButtonClicked() {
     this.isVisible = !this.isVisible;
   }
@@ -25,5 +26,13 @@ colorConverter = false;
     this.colorConverter = !this.colorConverter;
   }
 
-
+  onCurrencyButton(param: number) {
+    if (this.currencyItem <= 0 && param === -1) {
+      param = 0;
+    }
+    if (this.currencyItem >= (this.currencies.length - 1) && param === 1) {
+      param = 0;
+    }
+    this.currencyItem = this.currencyItem + param;
+  }
 }
