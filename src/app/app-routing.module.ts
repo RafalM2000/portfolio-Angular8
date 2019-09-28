cb5875a099acd6e858f2d9456ec3929e61c3d19b
Rafal <rmf@europe.com>
2019-09-28 07:22:37 +0200

    export of routing components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,4 +20,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
-export const routingComponents = [ DataBindingComponent, DirectivesComponent ]
+export const routingComponents = [ DataBindingComponent, DirectivesComponent ];
