7e249ddccae9894002907320da2e9ac2fbb4195c
Rafal <rmf@europe.com>
2019-10-20 21:39:09 +0200

    the tests for a child5 component

--- src/app/communication/child5/child5.component.spec.ts
+++ src/app/communication/child5/child5.component.spec.ts
@@ -1,5 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+
 import { Child5Component } from './child5.component';
 
 describe('Child5Component', () => {
@@ -8,7 +12,12 @@ describe('Child5Component', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ Child5Component ]
+      declarations: [ Child5Component ],
+      imports: [
+        BrowserAnimationsModule,
+        FormsModule,
+        MatInputModule
+      ]
     })
     .compileComponents();
   }));
