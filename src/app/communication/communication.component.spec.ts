31ced53686209d0e672c1af3261da063244f6768
Rafal <rmf@europe.com>
2019-10-29 20:52:35 +0100

    tests for ng-content interaction

--- src/app/communication/communication.component.spec.ts
+++ src/app/communication/communication.component.spec.ts
@@ -3,12 +3,14 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CommunicationComponent } from './communication.component';
 import { ParentComponent } from './parent/parent.component';
 import { Parent2Component } from './parent2/parent2.component';
+import { Parent3Component } from './parent3/parent3.component';
 import { ChildComponent } from './child/child.component';
 import { Child2Component } from './child2/child2.component';
 import { Child3Component } from './child3/child3.component';
 import { Child4Component } from './child4/child4.component';
 import { Child5Component } from './child5/child5.component';
 import { Child6Component } from './child6/child6.component';
+import { Child7Component } from './child7/child7.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
@@ -23,14 +25,16 @@ describe('CommunicationComponent', () => {
     TestBed.configureTestingModule({
       declarations: [
         CommunicationComponent,
-        ParentComponent,
         ChildComponent,
-        Parent2Component,
         Child2Component,
         Child3Component,
         Child4Component,
         Child5Component,
-        Child6Component
+        Child6Component,
+        Child7Component,
+        ParentComponent,
+        Parent2Component,
+        Parent3Component,
          ],
         imports: [
           BrowserAnimationsModule,
