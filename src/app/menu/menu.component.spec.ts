55e7cf36a12f82ed0ce59b98ee0c94957c7a52b7
Rafal <rmf@europe.com>
2019-11-23 07:21:56 +0100

    the unit tests

--- src/app/menu/menu.component.spec.ts
+++ src/app/menu/menu.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MenuComponent } from './menu.component';
+import { By } from 'protractor';
 
 describe('MenuComponent', () => {
   let component: MenuComponent;
@@ -22,4 +23,55 @@ describe('MenuComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show Home link', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelectorAll('a')[0].textContent).toEqual('Home');
+  }));
+
+  it('should show Data binding link', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelectorAll('a')[1].textContent).toEqual('Data binding');
+  }));
+
+  it('should show Directives link', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelectorAll('a')[2].textContent).toEqual('Directives');
+  }));
+
+  it('should show Pipes link', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelectorAll('a')[3].textContent).toEqual('Pipes');
+  }));
+
+  it('should show Pipes link', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelectorAll('a')[4].textContent).toEqual('Forms');
+  }));
+
+  it('should show Pipes link', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelectorAll('a')[5].textContent).toEqual('Communication');
+  }));
+
+  it('should show Pipes link', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelectorAll('a')[6].textContent).toEqual('Http');
+  }));
+
+  it('should show Pipes link', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelectorAll('a')[7].textContent).toEqual('Lifecycle hooks');
+  }));
+
+  it('should show Pipes link', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelectorAll('a')[8].textContent).toEqual('Lazy module');
+  }));
+
+  it('should show Pipes link', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelectorAll('a')[9].textContent).toEqual('Interceptor');
+  }));
+
 });
